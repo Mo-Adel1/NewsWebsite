0caad2ab0058c938a5cee61b47de748fec711237
Mo-Adel1 <102308283+Mo-Adel1@users.noreply.github.com>
2022-09-09 17:48:03 +0200

    Create Database Tables

---
+++ NewsWebsite/Models/ContactUS.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsWebsite.Models
+{
+    public class ContactUS
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string Message { get; set;}
+        public string Email { get; set; }   
+        public string Supject { get; set; } 
+    }
+}
---
+++ NewsWebsite/Models/Teammember.cs
@@ -0,0 +1,10 @@
+﻿namespace NewsWebsite.Models
+{
+    public class Teammember
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string JopTitle { get; set; }
+        public string Image { get; set; }   
+    }
+}
